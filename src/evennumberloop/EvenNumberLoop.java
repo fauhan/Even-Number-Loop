c4c1dd46035228b9a635cf25b2e3c5207bed5ac6
fauhan.pugar <fauhan.pugar@anabatic.com>
2019-02-25 20:32:51 +0700

    change limit value looping

--- src/evennumberloop/EvenNumberLoop.java
+++ src/evennumberloop/EvenNumberLoop.java
@@ -15,7 +15,7 @@ public class EvenNumberLoop {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        for (int i=1; i<=99; i++)
+        for (int i=1; i<=100; i++)
         {
             if (i%2 ==0)
                 System.out.print(i + " ");
